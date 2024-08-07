09b4117b23b095fd767b8ed6733dfeb621c352d4
MilanLR <milan@lariviereweb.com>
2024-08-07 13:54:07 +0200

    splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,19 @@
 import { ServerInfo } from "@/constants/Server";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { authenticatedFetch } from "@/app/fetch";
 import { useNavigation } from "expo-router";
 import { Colors, DefaultStyle } from "@/constants/Style";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function LoginScreen() {
   const navigation = useNavigation();
+
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
     const checkToken = async () => {
@@ -19,17 +24,34 @@ export default function LoginScreen() {
           routes: [{ name: "(tabs)" }],
         });
         navigation.navigate("(tabs)");
+        setAppIsReady(true);
       } else {
         navigation.reset({
           index: 0,
           routes: [{ name: "(tabs)" }, { name: "login" }],
         });
         navigation.navigate("login");
+        setAppIsReady(true);
       }
     };
 
     checkToken();
   }, [navigation]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
     <SafeAreaView
@@ -37,7 +59,7 @@ export default function LoginScreen() {
         DefaultStyle.view,
         { flex: 1, justifyContent: "center", alignItems: "center" },
       ]}
-    >
+      onLayout={onLayoutRootView}>
       <Text
         style={[
           DefaultStyle.viewTitle,
